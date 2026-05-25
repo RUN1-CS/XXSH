acce3ed7cc262c027dc212812df4c0b0b7bbe57c
Martincz7578 <martinek@rychly.cz>
2026-05-25 17:00:36 +0200

    Custom signals and infinite pipes

--- main.c
+++ main.c
@@ -19,11 +19,14 @@
 #ifndef MAIN_C
 #define MAIN_C
 
+#define _POSIX_C_SOURCE 200809L
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 #include <time.h>
+#include <signal.h>
 
 #include <sys/wait.h>
 
@@ -37,6 +40,7 @@
 
 #define MAX_COMMAND_LENGTH 1024
 
+// License notice, GPL 3.0 compatible
 void license_notice(){
     printf("XXSH  Copyright (C) 2026  RUN1/RUN1-CS\n");
     printf("This program comes with ABSOLUTELY NO WARRANTY; for details type `show w'.\n");
@@ -44,19 +48,46 @@ void license_notice(){
     printf("under certain conditions; type `show c' for details.\n");
 }
 
+// Signal handler (e.g., for Ctrl+C)
+void handle_sig(int sig){
+    if(sig == SIGINT){
+        printf("XX> ");
+        fflush(stdout);
+    }
+}
+
+// Setup signal handlers for the shell
+void setup_signals() {
+    struct sigaction sa;
+    sa.sa_handler = handle_sig;
+    sigemptyset(&sa.sa_mask);
+    sa.sa_flags = SA_RESTART; // Keeps system calls like read() from failing
+
+    // Handle Ctrl+C (Interrupt)
+    sigaction(SIGINT, &sa, NULL);
+    
+    // Handle 'kill' (Termination) - usually you want a clean exit here
+    sa.sa_handler = SIG_DFL; // Or a custom cleanup function
+    sigaction(SIGTERM, &sa, NULL);
+}
+
+// Check for special built-in commands that don't require forking
 bool specials(const char * command, const char *arg, bool *running){
     if(strcmp(command, "exit") == 0) { *running = false; return true; } // Exit command
 
+    // Oshi no Ko Easter Egg
     if(strcmp(command, "45510") == 0){
         printf("\033[1;35m[Even if it's a lie, I wanted it to be the truth.]\033[0m\n");
         return true;
     }
 
+    // Eighty Six Easter Egg
     if(strcmp(command, "86") == 0){
         printf("\033[1;31m[We will fight, and we will move forward.]\033[0m\n");
         return true;
     }
 
+    // Darling in the Franxx Easter Egg
     if(strcmp(command, "02") == 0){
         int rnd = rand() % 2;
         if(rnd == 0) printf("\033[1;95m[If you don't take a risk, you can't create a future.]\033[0m\n");
@@ -64,6 +95,7 @@ bool specials(const char * command, const char *arg, bool *running){
         return true;
     }
 
+    // Show command for license and credits (GPL 3.0 compatible)
     if(strcmp(command, "show") == 0){
         if(arg == NULL){
             license_notice();
@@ -86,10 +118,15 @@ bool specials(const char * command, const char *arg, bool *running){
     return false;
 }
 
+// Greeting message based on time of day
 void greeting(){
+    // Get current time
     time_t now = time(NULL);
+    // Convert to local time
     struct tm *t = localtime(&now);
+    // We just need hours
     int hour = t->tm_hour;
+    // Print greeting based on time of day
     if(hour >= 5 && hour < 12){
         printf("\033[1;32mGood morning! Welcome to XXSH.\033[0m\n");
     } else if(hour >= 12 && hour < 18){
@@ -99,95 +136,136 @@ void greeting(){
     }
 }
 
+// Execute a command with optional piping
 int execute_command(char *command, bool *running){
     char *args[MAX_COMMAND_LENGTH / 2 + 1]; // Maximum number of arguments
     int argc = 0;
     char *token = strtok(command, " \t");
 
+    // Tokenize the command into arguments
     while(token != NULL && argc < (MAX_COMMAND_LENGTH / 2)){
         args[argc++] = token;
         token = strtok(NULL, " \t");
     }
     args[argc] = NULL;
 
+    // If no command was entered, just return
     if(argc == 0) return 0;
 
     // Check for special built-in commands like exit/show before exec
     if(specials(args[0], (argc > 1) ? args[1] : NULL, running)) return 0;
 
-    // Pipe support for multiple stages: cmd1 | cmd2 | cmd3
+    // Check for pipes
+    int pipe_indices[argc];
     int pipe_count = 0;
-    for(int i = 0; i < argc; ++i){
-        if(strcmp(args[i], "|") == 0) ++pipe_count;
+    // Identify pipe positions and split it into separate commands
+    for(int i = 0; i < argc; i++){
+        if(strcmp(args[i], "|") == 0){
+            pipe_indices[pipe_count++] = i;
+            args[i] = NULL;
+        }
     }
 
+    // No pipes - simple command
     if(pipe_count == 0){
-        if(fork() == 0) { // Child process to execute the command
-            if(execvp(args[0], args) == -1){
-                printf("\033[1;31mBaka, command not found: %s\033[0m\n", args[0]);
-                _exit(127);
-            }
+        // Handle signals in the child process
+        pid_t pid = fork();
+        if(pid == 0){
+            execvp(args[0], args);
+            perror("execvp");
+            exit(EXIT_FAILURE);
+        } else if(pid > 0){
+            wait(NULL);
+        } else {
+            perror("fork");
+            return -1;
         }
-        wait(NULL); // Wait for the child process to finish
-    } else {
-        bool cleanup_triggered = false;
+        return 0;
+    }
 
-        int stage_count = pipe_count + 1;
-        char *cmds[stage_count][MAX_COMMAND_LENGTH / 2 + 1];
-        int cmd_argc[stage_count];
-        int stage = 0;
+    // Handle pipes without changing the shell's own stdin/stdout
+    int cmd_start = 0;
+    int prev_read = -1;
+    // Store child PIDs to wait for them later
+    pid_t pids[pipe_count + 1];
+    int pid_count = 0;
 
-        for(int i = 0; i < stage_count; ++i) cmd_argc[i] = 0;
+    // Loop through each command segment separated by pipes
+    for(int i = 0; i <= pipe_count; i++){
+        int fd[2] = {-1, -1};
+        // Create a pipe for all but the last command
+        if(i < pipe_count && pipe(fd) == -1){
+            perror("pipe");
+            return -1;
+        }
 
-        for(int i = 0; i < argc; ++i){
-            if(strcmp(args[i], "|") == 0){
-                cmds[stage][cmd_argc[stage]] = NULL;
-                ++stage;
+        // Fork a child process for the current command segment
+        pid_t pid = fork();
+        if(pid == 0){ // Child process
+
+            // Reset signal handlers to default in the child process
+            signal(SIGINT, SIG_DFL);
+            signal(SIGQUIT, SIG_DFL);
+            signal(SIGTSTP, SIG_DFL);
+            signal(SIGTTIN, SIG_DFL);
+            signal(SIGTTOU, SIG_DFL);
+            signal(SIGCHLD, SIG_DFL);
+
+            // If this is not the first command, set stdin to the previous pipe's read end
+            if(prev_read != -1){
+                dup2(prev_read, STDIN_FILENO);
+            }
+            // If this is not the last command, set stdout to the current pipe's write end
+            if(i < pipe_count){
+                dup2(fd[1], STDOUT_FILENO);
+            }
+
+            // Close unused file descriptors in the child process
+            if(prev_read != -1) close(prev_read);
+            if(i < pipe_count){
+                close(fd[0]);
+                close(fd[1]);
+            }
+
+            // Execute the command segment
+            execvp(args[cmd_start], &args[cmd_start]);
+            perror("execvp");
+            exit(EXIT_FAILURE);
+        } else if(pid > 0){ // Parent process
+            pids[pid_count++] = pid;
+
+            // Close unused file descriptors in the parent process
+            if(prev_read != -1) close(prev_read);
+            if(i < pipe_count){
+                close(fd[1]);
+                prev_read = fd[0];
             } else {
-                cmds[stage][cmd_argc[stage]++] = args[i];
-            }
-        }
-        cmds[stage][cmd_argc[stage]] = NULL;
-
-        int pipes[pipe_count][2];
-        for(int i = 0; i < pipe_count; ++i){
-            if(pipe(pipes[i]) == -1){
-                perror("pipe");
-                cleanup_triggered = true;
-                break;
-            }
-        }
-
-        if(!cleanup_triggered){
-            for(int i = 0; i < stage_count; ++i){
-                pid_t pid = fork();
-                if(pid == 0){
-                    if(i > 0) dup2(pipes[i - 1][0], STDIN_FILENO);
-                    if(i < pipe_count) dup2(pipes[i][1], STDOUT_FILENO);
-
-                    for(int j = 0; j < pipe_count; ++j){
-                        close(pipes[j][0]);
-                        close(pipes[j][1]);
-                    }
-
-                    if(execvp(cmds[i][0], cmds[i]) == -1){
-                        perror("Baka, command not found: ");
-                        _exit(127);
-                    }
-                }
+                prev_read = -1;
             }
 
-            for(int i = 0; i < pipe_count; ++i){
-                close(pipes[i][0]);
-                close(pipes[i][1]);
+            // Update cmd_start for the next command segment
+            cmd_start = (i < pipe_count) ? pipe_indices[i] + 1 : cmd_start;
+        } else {
+            // Fork failed
+            perror("fork");
+            if(prev_read != -1) close(prev_read);
+            if(i < pipe_count){
+                close(fd[0]);
+                close(fd[1]);
             }
-
-            for(int i = 0; i < stage_count; ++i) wait(NULL);
+            return -1;
         }
     }
+    
+    // Wait for all child processes
+    for(int i = 0; i < pid_count; i++){
+        waitpid(pids[i], NULL, 0);
+    }
+    
     return 0;
 }
 
+// Interpret (WIP) and run a script file
 void run_script(const char *filename){
     FILE *file = fopen(filename, "r");
     if(!file) return;
